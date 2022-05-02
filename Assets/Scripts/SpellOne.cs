b1ec002a3f2e9b4e89645f12ebd354833cf13b7a
Cláudio Araujo <claudiomarciofilho@hotmail.com>
2022-05-01 22:03:04 -0300

    Lógica de lançamento de feitiço, Spell 1 adicionada

--- Assets/Scripts/SpellOne.cs
+++ Assets/Scripts/SpellOne.cs
@@ -19,18 +19,19 @@ public class SpellOne : MonoBehaviour
 
     private void Move(){
         Vector3 movement = new Vector3(direction, 0f, 0f);
-        this.gameObject.transform.position += movement * Time.deltaTime * 10;
+        this.gameObject.transform.position += movement * Time.deltaTime * 13.5f;
     }
 
     private void Dismiss(){
-        if(Time.time > launchTime + 1.5f){
+        if(Time.time > launchTime + 2f){
             Destroy(this.gameObject);
         }
     }
 
     private void OnCollisionEnter2D(Collision2D other) {
-        if(other.gameObject.CompareTag(Tag.ENEMY) || other.gameObject.CompareTag(Tag.GROUND)){
+        if(other.gameObject.CompareTag(Tag.ENEMY)){
             Destroy(this.gameObject);
         }
+
     }
 }
