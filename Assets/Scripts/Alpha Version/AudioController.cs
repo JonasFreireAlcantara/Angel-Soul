68d5d97cd8e5d24da4e7df483548faa1136caae7
Augusto Miranda <83559528+augustocsmiranda@users.noreply.github.com>
2022-05-06 15:26:59 -0300

    AudioController/Source, MainTheme, Stage 6 Theme(Mammon), Stage 6 TileSets/map
    Audio Controller / Source
    Main Theme
    Estagio 6 tile, assets, Maps

---
+++ Assets/Scripts/Alpha Version/AudioController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public AudioSource audioSourceMusicaDeFundo;
+    public AudioClip[] musicasDeFundo;
+    void Start()
+    {
+        AudioClip musicasDeFundoDessaFase = musicasDeFundo[0];
+        audioSourceMusicaDeFundo.clip = musicasDeFundoDessaFase;
+        audioSourceMusicaDeFundo.loop = true;
+        audioSourceMusicaDeFundo.Play();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
