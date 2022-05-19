3cc0efd3ee46d97f41a4e4a69b6121915c7a4511
Augusto Miranda <83559528+augustocsmiranda@users.noreply.github.com>
2022-05-19 18:24:56 -0300

    DragonBonesAnimation
    Leviatan

---
+++ Assets/animationScript.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DragonBones;
+
+public class animationScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private UnityArmatureComponent leviatanAnim;
+    void Start()
+    {
+        
+        leviatanAnim = GetComponent<UnityArmatureComponent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        leviatanAnim.animation.Play("animtion0");
+    }
+}
