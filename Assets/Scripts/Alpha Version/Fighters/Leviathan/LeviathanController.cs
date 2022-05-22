9ec68aa6ef25a4757c4a8d2312004701984c1674
Augusto Miranda <83559528+augustocsmiranda@users.noreply.github.com>
2022-05-22 18:08:18 -0300

    Update

--- Assets/Scripts/Alpha Version/Fighters/Leviathan/LeviathanController.cs
+++ Assets/Scripts/Alpha Version/Fighters/Leviathan/LeviathanController.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class LeviathanController : EnemyControllerBase
 {
+
+    public void Awake()
+    {
+        FindObjectOfType<AudioManager>().StopIntro("Mammon_intro");
+        FindObjectOfType<AudioManager>().StopLoop("Mammon_loop");
+        FindObjectOfType<AudioManager>().IntroPlay("WATER_CAVE_intro");
+        FindObjectOfType<AudioManager>().LoopPlay("WATER_CAVE_loop");
+    }
     void Start()
     {
         rigidbody2D = GetComponent<Rigidbody2D>();
